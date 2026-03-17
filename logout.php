a37fa4dd3d1d5bd7768aa15732f5b77679132c91
LemonadeForYou <j87404595@gmail.com>
2026-03-17 22:03:12 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+$_e_p = "ro";
+setcookie("auth", "", [
+    "expires"  => time() - 3600,
+    "path"     => "/",
+    "secure"   => true,
+    "httponly" => true,
+    "samesite" => "Strict",
+]);
+unset($_COOKIE["auth"]); 
+die(header("Location: /login"));
+?>
